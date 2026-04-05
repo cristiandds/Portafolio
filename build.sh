90bf17f5494c3c6c687d56d20d9f59b6d569be01
cristian <jmcristian2@gmail.com>
2026-04-05 00:36:45 -0500

    crear usuario

--- build.sh
+++ build.sh
@@ -15,3 +15,14 @@ cd ..
 # Recolectar archivos estaticos en Django
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Crear superusuario automáticamente si no existe
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='tu_usuario').exists():
+    User.objects.create_superuser('cristian', 'cjulonmiranda@gmail.com', '2204Cr7')
+    print("Superusuario creado correctamente")
+else:
+    print("El superusuario ya existe")
+END
